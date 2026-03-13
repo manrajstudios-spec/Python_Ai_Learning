7d104edf3f1b7c50e8fa3d09a830bf9b71c56825
Manraj <manrajstudios@gmail.com>
2026-03-13 11:24:36 +0530

    strings

---
+++ Strings.py
@@ -0,0 +1,39 @@
+#print("Hello World")
+
+first_name = "Manraj"
+last_name = "Singh"
+
+_digits = "123"
+
+#print(first_name + " " + last_name)
+#print(first_name.lower())
+#print(first_name.upper())
+#print(first_name.find("M"))
+#print(first_name.isdigit())
+#print(first_name.replace("M" , "T"))
+
+#----slicing---------
+"""
+full_name = "Manraj Singh"
+
+f_name = full_name[0:6]
+
+step = full_name[2:11:2]
+
+reversed_name = full_name[11::-1]
+print(reversed_name)"""
+
+
+#website = "http://youtube.com"
+#site_name = website[7:14]
+#print(site_name)
+"""
+website = "http://youtube.com"
+slice = slice(7,-4)
+print(website[slice])
+
+site_name = website[7:-4]
+print(site_name)"""
+#--------------------
+
+
