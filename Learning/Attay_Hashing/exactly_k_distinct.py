6bd07be21fcb492eeb021be8a90760d1860b040b
Manraj Singh <manrajstudios@gmail.com>
2026-03-23 17:26:09 +0530

    Some PRoblems

--- Learning/Attay_Hashing/exactly_k_distinct.py
+++ Learning/Attay_Hashing/exactly_k_distinct.py
@@ -8,17 +8,17 @@ max_len = 0
 for r in range(len(a)):
     cur[a[r]] = cur.get(a[r],0)+1
 
-    while len(cur) != k:
-        if len(cur) > k:
-            cur[a[l]] -= 1
-            if cur[a[l]] == 0:
-                del cur[a[l]] 
+    while len(cur) > k:
+        cur[a[l]] -= 1
+        if cur[a[l]] == 0:
+            del cur[a[l]]             
         l+=1
     
-    if r-l+1 >max_len:
-        max_len = r-l+1
-        all = [a[l:r+1]]
-    elif r-l+1==max_len:
-        if not a[l:r+1] in all:all.append(a[l:r+1])
+    if len(cur) == k:
+        if r-l+1 >max_len:
+            max_len = r-l+1
+            all = [a[l:r+1]]
+        elif r-l+1==max_len:
+            if not a[l:r+1] in all:all.append(a[l:r+1])
 
 print(all)
