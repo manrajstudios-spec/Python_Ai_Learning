f5a55c6e29a0b4c0f4cc6908f6e3c6afdedf507e
Manraj Singh <manrajstudios@gmail.com>
2026-03-27 20:19:05 +0530

    Data Cleaning In Pandas

--- Learning/Basics/NumpyAndPandas/Pandas/Pandas.py
+++ Learning/Basics/NumpyAndPandas/Pandas/Pandas.py
@@ -1,5 +1,5 @@
 import pandas as pd
-"""
+
 #-------------SERIES-----------------------------
 
 #series is just a single column like data in single colomn of spreadsheet (1D)
@@ -211,54 +211,52 @@ print(poke_df["Height"].mean())
 group = poke_df.groupby("Type1")
 
 print(group["Height"].mean()) # We Get AVG Height Of Each Group Based n Type 1# all Other operations work too
-"""
+
 
 #<----------Data Cleaning---------->
 # --> Process of removing or fixing wrong data
 
-poke_df = pd.read_csv("Learning/Basics/NumpyAndPandas/pokemons.csv")
+poke_dex = pd.read_csv("Learning/Basics/NumpyAndPandas/pokemons.csv")
 
 # --> Using df.drop(colomn)
 
-# poke_df = poke_df.drop(columns=["Legendary","No"])
+poke_dex = poke_dex.drop(columns=["Legendary","No"])
 
-# print(poke_df)
+print(poke_dex)
 
 # Handle Missing Data
 # --> df.dropna(subset = [Coloumns We Need To Check]) NOTE --> It will remove the row which have null value for the coloumn mentioned 
 # Example --> Like Charmander is pure fire type so Type2 == null , SO if we include Type 2 in subset so charmander row will be dropped
 
-# poke_df = poke_df.dropna(subset=["Type2"])
+poke_dex = poke_dex.dropna(subset=["Type2"])
 
-# print(poke_df.to_string())
+print(poke_dex.to_string())
 
 # To Repalce Null values With Something
 # We Use df.fillna({"Coloumn_Name":"What We Need To Have Instead of Nan"})
 
-# poke_df = poke_df.fillna({"Type2":"Don't Have Another Type"}) # We Can Include Other coloumns too by adding more elments to dict
-# print(poke_df.to_string())
+poke_dex = poke_dex.fillna({"Type2":"Don't Have Another Type"}) # We Can Include Other coloumns too by adding more elments to dict
+print(poke_dex.to_string())
 
 # TO REPLACE INCONSISTANT VALUES OR WE CAN SAY CHANGE INSTANCES OF PARTICULAR VALUES
 # For Example lets change Grass --> GRASS
 
-# poke_df["Type1"] = poke_df["Type1"].replace({"Grass":"GRASS",
-#                                              "Fire":"FIRE"})
-# print(poke_df["Type1"]) # We Have GRASS instead of grass AND FIRE Instead of Fire
+poke_dex["Type1"] = poke_dex["Type1"].replace({"Grass":"GRASS",           
+                                             "Fire":"FIRE"})
+print(poke_dex["Type1"]) # We Have GRASS instead of grass AND FIRE Instead of Fire
 
 # Standarize Text
 
-poke_df["Name"] = poke_df["Name"].str.lower() # --> Change Every Name To lower Case
+poke_dex["Name"] = poke_dex["Name"].str.lower() # --> Change Every Name To lower Case
 
 # Data Types
 
-poke_df["Legendary"] = poke_df["Legendary"].astype(bool) # --> chnages 0 = False , 1 = True
+poke_dex["Legendary"] = poke_dex["Legendary"].astype(bool) # --> chnages 0 = False , 1 = True
 
 # Remove Duplicates 
 # For Example i added multiples instances of bulbasaur
 # using df.drop_duplicates() they will be removed
 
-poke_df = poke_df.drop_duplicates()
+poke_dex = poke_dex.drop_duplicates()
 
-print(poke_df.to_string())
-
-
+print(poke_dex.to_string())
---
+++ Learning/Basics/NumpyAndPandas/Pandas/Pd_Data_Cleaning.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+#<----------Data Cleaning---------->
+# --> Process of removing or fixing wrong data
+
+poke_df = pd.read_csv("Learning/Basics/NumpyAndPandas/pokemons.csv")
+
+# --> Using df.drop(colomn)
+
+poke_df = poke_df.drop(columns=["Legendary","No"])
+
+print(poke_df)
+
+# Handle Missing Data
+# --> df.dropna(subset = [Coloumns We Need To Check]) NOTE --> It will remove the row which have null value for the coloumn mentioned 
+# Example --> Like Charmander is pure fire type so Type2 == null , SO if we include Type 2 in subset so charmander row will be dropped
+
+poke_df = poke_df.dropna(subset=["Type2"])
+
+print(poke_df.to_string())
+
+# To Repalce Null values With Something
+# We Use df.fillna({"Coloumn_Name":"What We Need To Have Instead of Nan"})
+
+poke_df = poke_df.fillna({"Type2":"Don't Have Another Type"}) # We Can Include Other coloumns too by adding more elments to dict
+print(poke_df.to_string())
+
+# TO REPLACE INCONSISTANT VALUES OR WE CAN SAY CHANGE INSTANCES OF PARTICULAR VALUES
+# For Example lets change Grass --> GRASS
+
+poke_df["Type1"] = poke_df["Type1"].replace({"Grass":"GRASS",           
+                                             "Fire":"FIRE"})
+print(poke_df["Type1"]) # We Have GRASS instead of grass AND FIRE Instead of Fire
+
+# Standarize Text
+
+poke_df["Name"] = poke_df["Name"].str.lower() # --> Change Every Name To lower Case
+
+# Data Types
+
+poke_df["Legendary"] = poke_df["Legendary"].astype(bool) # --> chnages 0 = False , 1 = True
+
+# Remove Duplicates 
+# For Example i added multiples instances of bulbasaur
+# using df.drop_duplicates() they will be removed
+
+poke_df = poke_df.drop_duplicates()
+
+print(poke_df.to_string())
+
+
