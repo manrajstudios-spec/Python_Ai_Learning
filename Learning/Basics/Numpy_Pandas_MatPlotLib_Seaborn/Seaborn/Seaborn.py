9fd7612c18050de3c31ddaf6b43f01b877ce4ea4
Manraj Singh <manrajstudios@gmail.com>
2026-03-30 18:11:41 +0530

    Basic

--- Learning/Basics/Numpy_Pandas_MatPlotLib_Seaborn/Seaborn/Seaborn.py
+++ Learning/Basics/Numpy_Pandas_MatPlotLib_Seaborn/Seaborn/Seaborn.py
@@ -11,15 +11,48 @@ iris = sns.load_dataset("iris")
 planets = sns.load_dataset("planets")
 
 # %%
+# SCATTER PLOT
 tips
-sns.scatterplot(x='tip',y='total_bill',data=tips,hue="day",palette="YlGnBu")
+sns.scatterplot(x='tip',y='total_bill',data=tips,hue="day",palette="YlGnBu") # hue divides data based on provided
+# For Example above without hue data shows tips with total bill but with hue on day it will divide the info between days 
+
 
 # %%
+# BAR PLOT
+
 tips
-sns.barplot(x='sex',y='tip',data=tips,hue='day')
+sns.barplot(x='sex',y='tip',data=tips,hue='smoker')
+
 # %%
+# HIST PLOT
+
 sns.histplot(tips['tip'],bins=15,kde=True) # kde is distribution curve of data
+
+# %%
+#DISTRIBUTUON Plot
+
 sns.displot(tips["tip"],kde=True)
 
+# %%
+
+# Box Plot
+
+sns.boxenplot(x='day',y='tip',data=tips,hue='sex')
+# %%
+# STRIP PLOT
+
+sns.stripplot(x='day',y='tip',data=tips,hue='sex',dodge=True) # dodge separate hue condtions
+# %%
+# Joint Plots
+
+sns.jointplot(x='tip',y='total_bill',data=tips,kind='hex')
+# %%
+
+# PAIR PLOTS
+
+titanic
+sns.pairplot(titanic.select_dtypes(['number']),hue='pclass')
 
 # %%
+titanic.corr(numeric_only=True)
+sns.heatmap(titanic.corr(numeric_only=True),annot=True,cmap='icefire')
