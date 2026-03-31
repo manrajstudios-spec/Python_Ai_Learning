5d0218817cdaccc7ee3ded61ea97e068d88b7a62
Manraj Singh <manrajstudios@gmail.com>
2026-03-31 19:26:16 +0530

    Pandas

--- Learning/Basics/Scikit/Scikit.py
+++ Learning/Basics/Scikit/Scikit.py
@@ -1,5 +1,8 @@
 # %%
 
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -36,4 +39,30 @@ scalar = StandardScaler()
 X_train_scaled = scalar.fit_transform(X_train)
 X_test_scaled = scalar.transform(X_test)
 
+# %%
+knn = KNeighborsClassifier()
+knn.fit(X_train_scaled,y_train)
+
+# %%
+print(knn.score(X_test_scaled,y_test))
+# %%
+
+df:pd.DataFrame = load_breast_cancer(as_frame=True).frame
+df
+
+
+# %%
+#lets Try to visualize dataset
+
+for label in df.columns:
+    if label == 'target':continue
+
+    plt.hist(df[df['target'] == 1][label] , color="blue",density=True,alpha=0.7,label='Positive')
+    plt.hist(df[df['target'] == 0][label] , color="red",density=True,alpha=0.7,label='Negative')
+    plt.xlabel(label)
+    plt.ylabel("Probability")
+    plt.legend()
+
+    #sns.histplot(x=label,data=df,hue=df['target'],kde = True) # FOR USING SEABORN
+    plt.show()
 # %%
