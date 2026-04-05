cc9bc30500e95bd86e4287549adeafdf7f6b9ede
Manraj Singh <manrajstudios@gmail.com>
2026-04-05 22:43:47 +0530

    ML

--- Learning/MachineLearning/LinearReg/LinearRegression_Scikit.py
+++ Learning/MachineLearning/LinearReg/LinearRegression_Scikit.py
@@ -1,16 +1,17 @@
 # %% 
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Lasso
 
 # %%
 X,y = fetch_california_housing(return_X_y=True)
 
 # %%
-lrg = RandomForestClassifier(n_estimators=200)
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 
+lrg = Lasso(alpha=0.9)
 lrg.fit(X_train,y_train)
-
+lrg.score(X_test,y_test)
+# %%
 # %%
